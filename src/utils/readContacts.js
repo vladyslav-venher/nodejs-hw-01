2fea7d3af1ace035aac876ed566022a9586831f8
Vladyslav Venher <vladyslavvenher@gmail.com>
2024-10-19 15:45:42 +0300

    do h/w

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,7 @@
+import { readFile } from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  const data = await readFile(PATH_DB, 'utf8');
+  return JSON.parse(data);
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,6 @@
+import { writeFile } from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  await writeFile(PATH_DB, JSON.stringify(updatedContacts, 2, null));
+};
